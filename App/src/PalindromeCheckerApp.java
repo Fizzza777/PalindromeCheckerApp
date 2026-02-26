bf2bbbb614c50534f8431aed9cc7415afbf39644
Fizzza777 <fizatanzeem6@gmail.com>
2026-02-26 16:36:39 +0530

    Palindrome Checker App The objective of the PalindromeChecker App is to design and implement a console-based Java application that validates whether a given string is a palindrome under different conditions, while strengthening core programming fundamentals and data structure concepts.
    UC5: Stack-Based Palindrome Checker
    Goal: Use stack to reverse characters and validate palindrome.
    Flow:
    Push characters into stack
    Pop and compare
    Print result
    Key Concepts used in UC5:
    Stack – A linear data structure that follows the Last In First Out (LIFO) principle.
    Push Operation – Used to insert characters into the stack.
    Pop Operation – Used to remove characters from the stack in reverse order.
    Reversal Logic – Stack naturally reverses the order of elements, making it suitable for palindrome validation.
    Data Structure: Stack

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,43 +1,33 @@
-public class PalindromeCheckerApp {
+import java.util.Stack;
+
+public class UseCase5PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully");
-        System.out.println();
 
-        String word = "madam";
+        String input = "noon";
 
-        // Method 1: Reverse string method
-        String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        Stack<Character> stack = new Stack<>();
+
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
         }
 
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome (Checked using Reverse Method).");
-        } else {
-            System.out.println(word + " is NOT a Palindrome (Checked using Reverse Method).");
-        }
 
-        System.out.println();
-
-        // Method 2: Two-pointer comparison method
         boolean isPalindrome = true;
 
-        for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        if (isPalindrome) {
-            System.out.println(word + " is a Palindrome (Checked using Two-Pointer Method).");
-        } else {
-            System.out.println(word + " is NOT a Palindrome (Checked using Two-Pointer Method).");
-        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
