60d55fc3c62dcd60783be86632db7bc92a681091
Fizzza777 <fizatanzeem6@gmail.com>
2026-02-26 16:27:14 +0530

    Palindrome Checker App The objective of the PalindromeChecker App is to design and implement a console-based Java application that validates whether a given string is a palindrome under different conditions, while strengthening core programming fundamentals and data structure concepts.
    UC4: Character Array Based Palindrome Check
    Goal: Convert string to character array and compare characters.
    Flow:
    Convert string to char[]
    Use two-pointer approach
    Compare start & end characters
    Key Concepts used in UC4:
    Character Array (char[]) – A primitive array used to store individual characters of a string for index-based access.
    Array Indexing – Accessing elements using index positions starting from 0.
    Two-Pointer Technique – One pointer starts from the beginning and the other from the end, reducing unnecessary comparisons.
    Time Complexity Awareness – Demonstrates efficient comparison without creating extra objects.
    Data Structure: char[]

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,26 +1,31 @@
-public class PalindromeCheckerApp {
+public class UseCase4PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
-        System.out.println();
 
-        String word = "madam";
+        String input = "radar";
+
+
+        char[] chars = input.toCharArray();
+
+
+        int start = 0;
+        int end = chars.length - 1;
+
+
         boolean isPalindrome = true;
 
-        for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
                 isPalindrome = false;
                 break;
             }
+            start++;
+            end--;
         }
 
-        if (isPalindrome) {
-            System.out.println(word + " is a Palindrome.");
-        } else {
-            System.out.println(word + " is NOT a Palindrome.");
-        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
