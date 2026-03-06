f1a9509f8d7ffdea235685d071828c8c1135f67e
Fizzza777 <fizatanzeem6@gmail.com>
2026-03-06 14:06:52 +0530

    UC10: Case-Insensitive & Space-Ignored Palindrome Goal: Ignore spaces and case while checking a palindrome. Flow: Normalize string Apply previous logic Key Concepts used in UC10: String preprocessing Regular expressions Data Structure: String / Array

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,32 +1,25 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Declare and initialize the input string
-        String input = "radar";
+        Scanner sc = new Scanner(System.in);
 
-        // Convert the string into a character array
-        char[] chars = input.toCharArray();
+        System.out.print("Input: ");
+        String input = sc.nextLine();
 
-        // Initialize pointers
-        int start = 0;
-        int end = chars.length - 1;
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        // Assume palindrome initially
         boolean isPalindrome = true;
 
-        // Continue comparison until pointers cross
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        for (int i = 0; i < normalized.length() / 2; i++) {
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
-        // Required Output Format
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome: " + isPalindrome);
     }
 }
